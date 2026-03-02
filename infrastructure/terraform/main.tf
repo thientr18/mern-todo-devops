793f91b88402237e4dec06358d842eb32dae2aa8
Thien Tran <tranthien1882003@gmail.com>
2026-03-02 21:05:40 +0700

    update

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "vps" {
+  name   = var.droplet_name
+  region = var.region
+  size   = "s-1vcpu-2gb"
+  image  = "ubuntu-24-04-x64"
+
+  ssh_keys = [var.ssh_key]
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.vps.ipv4_address
+}
---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_key" {
+    description = "SSH Key to access the droplet"
+    type        = string
+    sensitive   = true
+}
+
+variable "region" {
+  default = "sgp1"
+}
+
+variable "droplet_name" {
+  default = "terraform-vps"
+}
